89bf47c73c639906faafc8633ef5d37f3909deeb
AnoshkoAlexey <AnoshkoAlexey@gmail.com>
2024-09-13 16:03:40 -0700

    Create complete solution

--- javaSwing/swingComponents/JMenu/MenuExample.java
+++ javaSwing/swingComponents/JMenu/MenuExample.java
@@ -1,12 +1,12 @@
 package javaSwing.swingComponents.JMenu;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.*;
 
 public class MenuExample extends JFrame {
+
     public MenuExample() {
         super("Menu Example");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,28 +14,31 @@ public class MenuExample extends JFrame {
         setLocationRelativeTo(null);
 
         JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
-
         JMenu fileMenu = new JMenu("File");
         fileMenu.setMnemonic(KeyEvent.VK_F);
-        menuBar.add(fileMenu);
 
-        JMenu newMenu = new JMenu("New");
-        JMenuItem textFileMenuItem = new JMenuItem("Text File");
-        JMenuItem imgFileMenuItem = new JMenuItem("Image File");
-        JMenuItem folderMenuItem = new JMenuItem("Folder");
-
-        newMenu.add(textFileMenuItem);
-        newMenu.add(imgFileMenuItem);
-        newMenu.add(folderMenuItem);
-        
-        fileMenu.add(newMenu);
-
+        JMenu newMenuItem = new JMenu("New");
         JMenuItem openMenuItem = new JMenuItem("Open");
         JMenuItem closeMenuItem = new JMenuItem("Close");
         JMenuItem closeAllMenuItem = new JMenuItem("Close All");
         JMenuItem exitMenuItem = new JMenuItem("Exit");
 
+        JMenuItem textFileMenuItem = new JMenuItem("Text File");
+        JMenuItem imgFileMenuItem = new JMenuItem("Image File");
+        JMenuItem folderMenuItem = new JMenuItem("Folder");
+
+        // you can rewrite it with a lambda if you prefer this
+        exitMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+
+        newMenuItem.add(textFileMenuItem);
+        newMenuItem.add(imgFileMenuItem);
+        newMenuItem.add(folderMenuItem);
+
+        fileMenu.add(newMenuItem);
         fileMenu.add(openMenuItem);
         fileMenu.add(closeMenuItem);
         fileMenu.add(closeAllMenuItem);
@@ -43,13 +46,14 @@ public class MenuExample extends JFrame {
         fileMenu.addSeparator();
         fileMenu.add(exitMenuItem);
 
-        exitMenuItem.addActionListener(event -> System.exit(0));
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
 
         setVisible(true);
+
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         new MenuExample();
     }
-    
 }
