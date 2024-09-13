cbd8f4d495d87238e3a012306abfa9f2355af359
AnoshkoAlexey <AnoshkoAlexey@gmail.com>
2024-09-13 15:41:06 -0700

    Create Menu items

--- javaSwing/swingComponents/JMenu/MenuExample.java
+++ javaSwing/swingComponents/JMenu/MenuExample.java
@@ -1,6 +1,10 @@
 package javaSwing.swingComponents.JMenu;
 
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MenuExample extends JFrame {
     public MenuExample() {
@@ -8,7 +12,30 @@ public class MenuExample extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setLocationRelativeTo(null);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(fileMenu);
+
+        JMenuItem newMenuItem = new JMenuItem("New");
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        JMenuItem closeMenuItem = new JMenuItem("Close");
+        JMenuItem closeAllMenuItem = new JMenuItem("Close All");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+
+        fileMenu.add(newMenuItem);
+        fileMenu.add(openMenuItem);
+        fileMenu.add(closeMenuItem);
+        fileMenu.add(closeAllMenuItem);
+
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+
         setVisible(true);
+
     }
 
     public static void main(String[] args) {
