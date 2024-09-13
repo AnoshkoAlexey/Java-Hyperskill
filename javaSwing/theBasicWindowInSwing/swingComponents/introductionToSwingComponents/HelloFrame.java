376b256a0f8c6bd338d2d86d2430022662fced01
AnoshkoAlexey <AnoshkoAlexey@gmail.com>
2024-09-13 14:04:05 -0700

    Add JText Field

--- javaSwing/theBasicWindowInSwing/swingComponents/introductionToSwingComponents/HelloFrame.java
+++ javaSwing/theBasicWindowInSwing/swingComponents/introductionToSwingComponents/HelloFrame.java
@@ -2,6 +2,7 @@ package javaSwing.theBasicWindowInSwing.swingComponents.introductionToSwingCompo
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class HelloFrame extends JFrame{
 
@@ -11,15 +12,21 @@ public class HelloFrame extends JFrame{
         setSize(300, 300);
         setLocationRelativeTo(null);
         setLayout(null);
-        setVisible(true);
 
         JLabel nameLabel = new JLabel();
         nameLabel.setText("Your Name");
         nameLabel.setBounds(40, 20, 100, 30);
         add(nameLabel);
+
+        JTextField nameTextField = new JTextField();
+        nameTextField.setBounds(160, 20, 100, 30);
+        add(nameTextField);
+        setVisible(true);
     }
     
     public static void main(String[] args) {
         new HelloFrame();
     }
+
+
 }
