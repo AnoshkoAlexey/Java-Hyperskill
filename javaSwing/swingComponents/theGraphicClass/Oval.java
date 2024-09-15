2939f53c11b4e4da49d55f12d286e351658af86e
AnoshkoAlexey <AnoshkoAlexey@gmail.com>
2024-09-14 17:28:45 -0700

    Add a class that draws oval

---
+++ javaSwing/swingComponents/theGraphicClass/Oval.java
@@ -0,0 +1,27 @@
+package javaSwing.swingComponents.theGraphicClass;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+public class Oval extends JFrame {
+
+    public static void main(String[] args) {
+        Oval drawing = new Oval();
+        drawing.setVisible(true);
+    }
+
+    public Oval() {
+        super("Oval Picture");
+
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(Color.white);
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawOval(250, 250, 50, 50);
+    }
+    
+}
