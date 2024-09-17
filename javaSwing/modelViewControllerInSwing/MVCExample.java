057ee5955980e98cc91af000ce9ae5c62cfb2969
AnoshkoAlexey <AnoshkoAlexey@gmail.com>
2024-09-16 19:34:57 -0700

    Add corrections to MVC examples

--- javaSwing/modelViewControllerInSwing/MVCExample.java
+++ javaSwing/modelViewControllerInSwing/MVCExample.java
@@ -2,6 +2,7 @@ package javaSwing.modelViewControllerInSwing;
 
 public class MVCExample {
     public static void main(String[] args) {
-        View view = new View();
+        Controller controller = new Controller(new Model(1), new View());
+        controller.control();
     }
 }
