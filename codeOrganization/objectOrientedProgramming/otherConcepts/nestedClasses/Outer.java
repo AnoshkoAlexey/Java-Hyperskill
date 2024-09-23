7e891b2c8053a508bd17f168d9bfef12001a897f
AnoshkoAlexey <AnoshkoAlexey@gmail.com>
2024-09-23 15:31:42 -0700

    Add an example of a local inner class

---
+++ codeOrganization/objectOrientedProgramming/otherConcepts/nestedClasses/Outer.java
@@ -0,0 +1,22 @@
+package codeOrganization.objectOrientedProgramming.otherConcepts.nestedClasses;
+
+public class Outer {
+    private int number = 10;
+
+    void someMethod() {
+
+        class LocalInner {
+            private void print() {
+                System.out.println("Number = " + Outer.this.number);
+            }
+        }
+
+        LocalInner inner = new LocalInner();
+        inner.print();
+    }
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.someMethod();
+    }
+}
