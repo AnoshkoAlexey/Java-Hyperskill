41e4b9bf9b9df60f2a74ef425cbcb78ecc1d0afe
AnoshkoAlexey <AnoshkoAlexey@gmail.com>
2024-09-23 15:21:51 -0700

    Add an example with a scope of a static nested classes

--- codeOrganization/objectOrientedProgramming/otherConcepts/nestedClasses/Painting.java
+++ codeOrganization/objectOrientedProgramming/otherConcepts/nestedClasses/Painting.java
@@ -4,8 +4,26 @@ public class Painting {
     
     private String name;
 
-    public static class Sketch {
+    private static double length;
+    private static double width;
 
+    public static void setLength(double length) {
+        Painting.length = length;
+    }
+
+    public static void setWidth(double width) {
+        Painting.width = width;
+    }
+
+    public Painting(String name, double length, double width) {
+        this.name = name;
+        this.length = length;
+        this.width = width;
+    }
+
+
+    public static class Sketch {
+     
         private int id;
 
         public Sketch(int id) {
@@ -18,7 +36,11 @@ public class Painting {
         }
 
         private void drawForest() {
-            System.out.println("Forest was drawn in a sketch!");
+            if (Painting.length > 5 && Painting.width > 3) {
+                System.out.println("Big forest was drawn in a sketch!");
+            } else {
+                System.out.println("Small forest was drawn in a sketch!");
+            }
         }
 
         private void drawBear() {
