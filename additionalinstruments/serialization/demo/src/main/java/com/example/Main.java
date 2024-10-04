5525d2303d0d0568a15f8fa0cc95aab5f80d5ba4
Anoshko Alexey <anoshkoalexey@gmail.com>
2024-10-03 20:39:58 -0700

    Add the rest of examples

--- additionalinstruments/serialization/demo/src/main/java/com/example/Main.java
+++ additionalinstruments/serialization/demo/src/main/java/com/example/Main.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
@@ -28,5 +29,38 @@ public class Main {
         String postAsString2 = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(post);
         
         System.out.println(postAsString2);
+
+        String inputJson = "{\"id\":1, \"createdDate\":1654027200000, \"content\":\"I learned how to use jackson!\", \"likes\":10, \"comments\":[\"Well done!\",\"Great job!\"]}\n";
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        // Post post3 = objectMapper2.readValue(inputJson, Post.class);
+        // System.out.println(post3);
+
+        // Serialization of Lists of objects
+        List<Post> posts = List.of(
+            new Post(1, new Date(), "Content1", 10, Arrays.asList("Comment1", "Comment2")),
+            new Post(2, new Date(), "Content2", 20, Arrays.asList("Comment3", "Comment4"))
+        );
+
+        String postsJson = objectMapper.writerWithDefaultPrettyPrinter()
+            .writeValueAsString(posts);
+        System.out.println(postsJson);
+
+        // Deserialization of Lists of objects
+        List<Post> deserializedPosts = objectMapper.readValue(postsJson, List.class);
+        System.out.println(deserializedPosts);
+
+        // Serialization of composed objects
+        Post post4 = new Post(1, new Date(), "Content1", 10, Arrays.asList("Comment1", "Comment2"),
+            new Person("Alice", 20));
+        String postJson = objectMapper.writerWithDefaultPrettyPrinter()
+            .writeValueAsString(post4);
+        System.out.println(postJson);
+
+        // Deserialization of composed objects 
+        //Post deserializedPosts2 = objectMapper.readValue(postJson, Post.class);
+        //System.out.println(deserializedPosts2);
+    
+        
+    
     }
 }
--- additionalinstruments/serialization/demo/src/main/java/com/example/Post.java
+++ additionalinstruments/serialization/demo/src/main/java/com/example/Post.java
@@ -6,7 +6,14 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonPropertyOrder({
+    "likes",
+    "comments",
+    "createdDate", // here you can also use 'postedAt'
+    "content",
+})
 public class Post {
 
     @JsonIgnore
@@ -17,6 +24,7 @@ public class Post {
     private String content;
     private int likes;
     private List<String> comments;
+    private Person person;
 
     // constructor, getters, setters    
     public Post(int id, Date createDate, String content, int likes, List<String> comments) {
@@ -25,6 +33,18 @@ public class Post {
         this.content = content;
         this.likes = likes;
         this.comments = comments;
+    }
+
+    public Post() {}
+
+    public Post(int id, Date createDate, String content, int likes, List<String> comments, Person person) {
+        //TODO Auto-generated constructor stub
+        this.id = id;
+        this.createDate = createDate;
+        this.content = content;
+        this.likes = likes;
+        this.comments = comments;
+        this.person = person;
     }
 
     public int getId() {
